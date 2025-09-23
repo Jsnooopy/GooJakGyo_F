de1d8168f6d6f44a7570a56937d99ae09f0aaf03
Jae <lijae3552@gmail.com>
2025-09-23 16:32:23 +0900

    Group Chat List & Dynamic processing of roomId

--- src/views/StompChatPage.vue
+++ src/views/StompChatPage.vue
@@ -44,11 +44,13 @@ export default{
             newMessage: "",
             stompClient: null,
             token: "",
-            senderEmail: null
+            senderEmail: null,
+            roomId: null
         }
     },
     created(){
         this.senderEmail = localStorage.getItem("email");
+        this.roomId = this.$route.params.roomId;
         this.connectWebsocket();
     },
     // 사용자가 현재 라우트에서 다른 라우트로 이동하려고 할 때 호출되는 훅 함수
@@ -73,7 +75,7 @@ export default{
                 Authorization: `Bearer ${this.token}`
             },
                 () => {
-                    this.stompClient.subscribe(`/topic/1`, (message) => {
+                    this.stompClient.subscribe(`/topic/${this.roomId}`, (message) => {
                         const parseMessage = JSON.parse(message.body);
                         this.messages.push(parseMessage);
                         this.scrollToBottom();
@@ -87,7 +89,7 @@ export default{
                 senderEmail: this.senderEmail,
                 message: this.newMessage
             }
-            this.stompClient.send(`/publish/1`, JSON.stringify(message));
+            this.stompClient.send(`/publish/${this.roomId}`, JSON.stringify(message));
             this.newMessage = ""
         },
         scrollToBottom(){
@@ -98,7 +100,7 @@ export default{
         },
         disconnectWebSocket(){
             if(this.stompClient && this.stompClient.connected){
-                this.stompClient.unsubscribe(`/topic/1`);
+                this.stompClient.unsubscribe(`/topic/${this.roomId}`);
                 this.stompClient.disconnect();
             }
         }
